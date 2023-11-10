891f9762eb5d5176e08ab9e1c1717237f789f81d
Tymek <Tymoteusz.91100@gmail.com>
2023-11-10 19:34:44 +0100

    zmieniono szachownicę na tablicę dwuwymiarową

--- src/Szachownica.java
+++ src/Szachownica.java
@@ -1,51 +1,45 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Szachownica
-{
-    int x=1;
-    int y=1;
+public class Szachownica {
+    int x = 1;
+    int y = 1;
     List<Integer> wspolrzedne;
     boolean pionyRozstawione = false;
 
-    public Szachownica()
-    {
+    public Szachownica() {
         rysujSzachownice();
     }
 
-    int[] notacja = {1, 2, 3, 4, 5, 6, 7, 8};
-    int z = 8;
-    public void rysujSzachownice()
-        {
-            for (int i=0; i<8; i++)
-            {
+    char[][] szachownica = new char[8][8];
+    char[] notacja = {'8', '7', '6', '5', '4', '3', '2', '1'};
 
-                    for (int j = 0; j < 8; j++)
-                    {
-                        List<Integer> wspolrzedne = new ArrayList<Integer>();
-                        wspolrzedne.add(i+1);
-                        wspolrzedne.add(j+1);
-                        if ((i + j) % 2 == 0) {
-                            System.out.print("■ ");
-                        } else {
-                            System.out.print("□ ");
-                        }
-                        if (j == 7) {
-                            z--;
-                            System.out.print(notacja[z]);
-                        }
-                        //System.out.println("|"+wspolrzedne.get(0)+"=x "+wspolrzedne.get(1)+"=y|");
-                    }
-
-                System.out.println();
-            }
-            System.out.println("A B C D E F G H");
-        }
-        public void rozstawFiguryGraczy()
-        {
-            for (int i = 0; i < 8; i++) {
-                Pion pionek = new Pion();
-                System.out.print(pionek.getZnak()+" ");
+    public void rysujSzachownice() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                szachownica[i][j] = (i + j) % 2 == 0 ? '■' : '□';
             }
         }
+        printSzachownica();
+    }
+
+    public void printSzachownica() {
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print(szachownica[i][j] + " ");
+                if (j == 7) {
+                    System.out.print(notacja[i]);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("A B C D E F G H");
+    }
+
+    public void rozstawFiguryGraczy() {
+        for (int i = 0; i < 8; i++) {
+            Pion pionek = new Pion();
+            System.out.print(pionek.getZnak() + " ");
+        }
+    }
 }
